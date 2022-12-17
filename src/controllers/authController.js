021a23f21265a3ca29fa84b2b4c24bab3e3927c4
Maristane Figueiredo <maristanetecnologia@gmail.com>
2022-12-17 18:31:46 -0300

    fix:signUP

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,41 +1,31 @@
 import bcrypt from "bcrypt";
 
-
 import connectionDB from "../database/db.js";
 
 export async function postSignUp(req, res) {
-   // const user = res.locals.user;
-   const {name, password, email} = res.locals.user;
-   const passwordHash = bcrypt.hashSync(password, 10)
+  // const user = res.locals.user;
+  const { name, password, email } = res.locals.user;
+  const passwordHash = bcrypt.hashSync(password, 10);
 
-       
-   console.log('entrei aqui - name, passwordHash, email', name,passwordHash, email)
-    try{
+  try {
+    await connectionDB.query(
+      `
+            INSERT INTO users ( name, password, email) VALUES($1, $2, $3)
+            `,
+      [name, passwordHash, email]
+    );
 
-        await connectionDB.query(
-            `
-            INSERT INTO users ( name, password, email) VALUES($1, $2, $3);
-            `
-        ,[name, passwordHash, email]
-        )
-        console.log('entrei aqui2')
-        
-        res.sendStatus(201)
-
-    } catch(error) {
-        console.log(error)
-        return res.status(500).send({message: "Erro inesperado no servidor!"})
-    }
+    res.sendStatus(201);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({ message: "Erro inesperado no servidor!" });
+  }
 }
 
-
-
 export async function postSignIn(req, res) {
-    try{
-
-    } catch(error) {
-        console.log(error)
-        return res.status(500).send({message: "Erro no servidor!"})
-
-    }
+  try {
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({ message: "Erro no servidor!" });
+  }
 }
--- src/database/db.js
+++ src/database/db.js
@@ -1,14 +1,32 @@
 import pg from "pg";
+import dotenv from "dotenv";
+dotenv.config();
 
 const { Pool } = pg;
 
+//banco render
 const connectionDB = new Pool({
   connectionString: process.env.DATABASE_URL,
-  //ssl: true,
-  ssl: false,
+  ssl: true,
+  //ssl: false,
 });
 
+//banco local
+// const connectionDB = new Pool({
+//   host: "localhost",
+//   port: 5432,
+//   user: "postgres",
+//   password: "Banco2404@",
+//   database: "shortly",
+// });
 
+//banco local
+// const connectionDB = new Pool({
+//   host: process.env.HOST,
+//   port: process.env.PORTDB,
+//   user: process.env.USERDB,
+//   password: process.env.PASSWORD,
+//   database: process.env.DATABASE,
+// });
 
-
-export default connectionDB
+export default connectionDB;
